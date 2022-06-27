9fd4ad95ca09a7ab605466314a6b7beea71bf839
SDMasterGames <matheus.geraldo1@outlook.com>
2022-06-27 18:36:44 -0400

    add: exportação do casos de usos para shopCart

--- src/useCases/index.js
+++ src/useCases/index.js
@@ -1,6 +1,8 @@
 const products = require("./products");
 const users = require("./users");
+const shopcart = require("./shopcart");
 module.exports = {
   products,
-  users
+  users,
+  shopcart,
 };
