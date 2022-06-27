dcb4416eaf8cf8f591e823f87fb62ad34c308338
SDMasterGames <matheus.geraldo1@outlook.com>
2022-06-27 18:38:14 -0400

    carrinho de compra

---
+++ models/shopcart.js
@@ -0,0 +1,35 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class shopcart extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  shopcart.init(
+    {
+      productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "products",
+          key: "id",
+        },
+      },
+      qtd: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "shopcart",
+    }
+  );
+  return shopcart;
+};
