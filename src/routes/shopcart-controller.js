aae6fe4d295dd78427be2e74ffbd6680b12c5d76
SDMasterGames <matheus.geraldo1@outlook.com>
2022-06-28 12:55:58 -0400

    add: controllers para criação e remoção

--- src/routes/shopcart-controller.js
+++ src/routes/shopcart-controller.js
@@ -15,4 +15,42 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const { id, qtd } = req.body;
+    const result = await shopcart.addCart({
+      id,
+      qtd,
+    });
+    return res.status(201).send(result);
+  } catch (error) {
+    return res.status(400).send({
+      error: error.message || "Erro inesperado ocorreu",
+    });
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    const { id } = req.query;
+    const result = await shopcart.removeProductCart(id);
+    return res.status(200).send(result);
+  } catch (error) {
+    return res.status(400).send({
+      error: error.message || "Erro inesperado ocorreu",
+    });
+  }
+});
+
+router.delete("/clear", async (req, res) => {
+  try {
+    const result = await shopcart.deleteCart();
+    return res.status(200).send(result);
+  } catch (error) {
+    return res.status(400).send({
+      error: error.message || "Erro inesperado ocorreu",
+    });
+  }
+});
+
 module.exports = (app) => app.use("/cart", router);
